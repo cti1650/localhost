ce0a4f3e8234fcb03822a4efd7c565740661e7af
cti1650 <cti1650@gmail.com>
2022-02-12 01:52:35 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Localhost Page Cheker
+
+## 参考にしたサイト
+- [iframe でWebページを埋め込んで縮小表示する（レスポンシブ対応） | フロントエンドエンジニアのblog – 有限会社Willさんいん](https://www.will3in.co.jp/frontend-blog/article/shrink-webpage-with-iframe-responsive/)  
---
+++ src/script.js
@@ -0,0 +1,12 @@
+jQuery(function ($) {
+  $(window).on("load resize", function () {
+    var pc_w = $(".pc .frame").width() / 1280;
+    $(".pc iframe").css("transform", "scale(" + pc_w + ")");
+
+    var tb_w = $(".tb .frame").width() / 768;
+    $(".tb iframe").css("transform", "scale(" + tb_w + ")");
+
+    var sp_w = $(".sp .frame").width() / 375;
+    $(".sp iframe").css("transform", "scale(" + sp_w + ")");
+  });
+});
